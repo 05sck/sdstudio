9340a129ed6a62eb8ea80da394a6f22cb04c8976
youngho <macbookpro14@macbookpro14ui-MacBookPro.local>
2024-11-09 12:15:13 +0900

    reservation correction demo commit

--- lib/Screens/reservation_screen.dart
+++ lib/Screens/reservation_screen.dart
@@ -5,6 +5,6 @@ class ReservationScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text("Reservation");
+    return const Text('Hello Branch');
   }
 }
